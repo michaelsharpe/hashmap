514f67dda328165ba575a1fab634da244d7aa163
michaelsharpe <m.sharpe@project89.org>
2013-12-09 15:20:01 -0500

    filled out base functionality of geomarkerscontroller and related views

--- app/controllers/geomarkers_controller.rb
+++ app/controllers/geomarkers_controller.rb
@@ -1,36 +1,46 @@
 class GeomarkersController < ApplicationController
   def index
-  
+    @geomarker = Geomarker.all
   end
   
   def show
-    
+    @geomarker = Geomarker.find(params[:id])
   end
   
   def new
-    
-  end
-  
-  def edit
-    
+    @geomarker = Geomarker.new
   end
   
   def create
-    
+    @geomarker = Geomarker.new(geomarker_params)
+    if @geomarker.save
+      redirect_to geomarker_path(@geomarker)
+    else
+      render "new"
+    end
   end
   
+  def edit
+    @geomarker = Geomarker.find(params[:id])
+  end
   
   def update
-    
+    @geomarker = Geomarker.find(params[:id])
+    if @geomarker.update_attributes(geomarker_params)
+      redirect_to geomarker_path(@geomarker)
+    else
+      render "edit"
+    end
   end
   
   def destroy
-    
+    @geomarker = Geomarker.find(params[:id])
+    @geomarker.destroy
+    redirect_to geomarkers_path
   end
 
   private
   def geomarker_params
     params.require(:geomarker).permit(:name, :description, :latitude, :longitude)
   end
-
 end
