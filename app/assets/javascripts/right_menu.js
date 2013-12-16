92505ef9fc76e0b029e8502a976ccc6d60666fdb
michaelsharpe <m.sharpe@project89.org>
2013-12-16 15:02:47 -0500

    Current tags populate a variable based on menu interaction.

--- app/assets/javascripts/right_menu.js
+++ app/assets/javascripts/right_menu.js
@@ -1,20 +1,20 @@
-var tags = [];
-var active_collection;
-var $menu = $('#right-menu')
+var currentTags = [];
+var activeCollection;
+var $menu = $('#right-menu');
+var menuOptions = {};
 
 $(document).ready(function(){
   // Load mmenu
+  $menu.mmenu(menuOptions);
   
-  $menu.mmenu({
-  });
   //Only one collection at a time is set to visible 
   $(".collection-eye").on('click', function(){
     // Go through each collection and hide it
     $(".collection-eye").each( function(){
-      $(this).attr("data-show", "disabled");
+      $(this).parent().parent().attr("data-show", "disabled");
       $(this).removeClass("fa-eye").addClass("fa-eye-slash");
       $(this).children().each(function(){
-        $(this).attr("data-show", "disabled");
+        $(this).parent().parent().attr("data-show", "disabled");
         $(this).removeClass("fa-eye").addClass("fa-eye-slash");
       });
       // Go into each collection and hide all its tags
@@ -23,7 +23,7 @@ $(document).ready(function(){
       $(href).find("li").first().parent().attr("data-active", "false")
       $(href).find("li").not($(".mm-subclose")).each(function(){
         $(this).find(".tag-eye").each(function(){
-          $(this).attr("data-show", "disabled");
+          $(this).parent().parent().attr("data-show", "disabled");
           $(this).removeClass("fa-eye").addClass("fa-eye-slash");
         })
       })
@@ -34,23 +34,45 @@ $(document).ready(function(){
     $(href).find("li").first().parent().attr("data-active", "true")
     $(href).find("li").not($(".mm-subtitle")).each(function(){
       $(this).find(".tag-eye").each(function(){
-        $(this).attr("data-show", "enabled");
+        $(this).parent().parent().attr("data-show", "enabled");
         $(this).removeClass("fa-eye-slash").addClass("fa-eye");
-        active_collection = href
+        activeCollection = href
       })
     })
     // Make this collection visible
     $(this).toggleClass("fa-eye fa-eye-slash");
-    $(this).attr("data-show","enabled");
+    $(this).parent().parent().attr("data-show","enabled");
+    var element = $(this).parent().parent()
+    setCollectionTags(element);
   });
+
   // Individual tags within collections can all be visible or hidden
   $(".tag-eye").on('click', function(){
     var current_collection = "#" + $(this).parent().parent().parent().attr("id")
-    if(current_collection == active_collection){
+    if(current_collection == activeCollection){
       $(this).toggleClass("fa-eye fa-eye-slash");
-    $(this).attr("data-show", $(this).attr("data-show") == "disabled" ? "enabled" : "disabled");
+    $(this).parent().parent().attr("data-show", $(this).parent().parent().attr("data-show") == "disabled" ? "enabled" : "disabled");
     }
+    var element = $(this).parent().parent();
+    updateTags();
   })
-  // Iterate through each tag in a collection list and load only those markers.
-
 });
+
+// Used to set the collection of current tags
+var setCollectionTags = function(element){
+  if( $(element).attr("data-show") == "enabled"){
+    updateTags();
+  }
+};
+
+// Update current tags
+var updateTags = function() {
+  tags = []
+  $("#right-menu").children().each(function(){
+    $(this).children().each(function(){
+      if ($(this).attr("data-tag") !== undefined && $(this).attr("data-show") == "enabled") {
+      tags.push($(this).attr("data-tag"))
+      }
+    })
+  })
+};
