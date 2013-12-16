edbd941ae590707bc963806979ab5eecaa5c556f
jmacpherson <jd.macpherson@gmail.com>
2013-12-16 15:49:03 -0500

    changed timing of updateMap after submission of new geomarker

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -176,6 +176,7 @@ function removeMarkersOutsideOfMapBounds() {
 function makeMarker(lat, lng, markerJSON){
   var latlng = L.latLng(lat, lng);
   var marker = L.marker(latlng,{title: markerJSON.name});
+  marker.bindPopup("<div class='marker-popup'><p>Name: " + markerJSON.name + "</p><br><img src='" + markerJSON.image + "' /></div>");
   marker.addTo(map);
   if (newMode) {
     marker.setOpacity(0.5);
@@ -184,55 +185,55 @@ function makeMarker(lat, lng, markerJSON){
 }
 
 // Simulating asynchronous file upload
-function fileUpload(form, action_url, div_id) {
-  // Create the iframe...
-  var iframe = document.createElement("iframe");
-  iframe.setAttribute("id", "upload_iframe");
-  iframe.setAttribute("name", "upload_iframe");
-  iframe.setAttribute("width", "0");
-  iframe.setAttribute("height", "0");
-  iframe.setAttribute("border", "0");
-  iframe.setAttribute("style", "width: 0; height: 0; border: none;");
+// function fileUpload(form, action_url, div_id) {
+//   // Create the iframe...
+//   var iframe = document.createElement("iframe");
+//   iframe.setAttribute("id", "upload_iframe");
+//   iframe.setAttribute("name", "upload_iframe");
+//   iframe.setAttribute("width", "0");
+//   iframe.setAttribute("height", "0");
+//   iframe.setAttribute("border", "0");
+//   iframe.setAttribute("style", "width: 0; height: 0; border: none;");
 
-  // Add to document...
-  form.parentNode.appendChild(iframe);
-  window.frames['upload_iframe'].name = "upload_iframe";
+//   // Add to document...
+//   form.parentNode.appendChild(iframe);
+//   window.frames['upload_iframe'].name = "upload_iframe";
 
-  iframeId = document.getElementById("upload_iframe");
+//   iframeId = document.getElementById("upload_iframe");
 
-  // Add event...
-  var eventHandler = function () {
+//   // Add event...
+//   var eventHandler = function () {
 
-          if (iframeId.detachEvent) iframeId.detachEvent("onload", eventHandler);
-          else iframeId.removeEventListener("load", eventHandler, false);
+//           if (iframeId.detachEvent) iframeId.detachEvent("onload", eventHandler);
+//           else iframeId.removeEventListener("load", eventHandler, false);
 
-          // Message from server...
-          if (iframeId.contentDocument) {
-              content = iframeId.contentDocument.body.innerHTML;
-          } else if (iframeId.contentWindow) {
-              content = iframeId.contentWindow.document.body.innerHTML;
-          } else if (iframeId.document) {
-              content = iframeId.document.body.innerHTML;
-          }
+//           // Message from server...
+//           if (iframeId.contentDocument) {
+//               content = iframeId.contentDocument.body.innerHTML;
+//           } else if (iframeId.contentWindow) {
+//               content = iframeId.contentWindow.document.body.innerHTML;
+//           } else if (iframeId.document) {
+//               content = iframeId.document.body.innerHTML;
+//           }
 
-          document.getElementById(div_id).innerHTML = content;
+//           document.getElementById(div_id).innerHTML = content;
 
-          // Del the iframe...
-          setTimeout('iframeId.parentNode.removeChild(iframeId)', 250);
-      }
+//           // Del the iframe...
+//           setTimeout('iframeId.parentNode.removeChild(iframeId)', 250);
+//       }
 
-  if (iframeId.addEventListener) iframeId.addEventListener("load", eventHandler, true);
-  if (iframeId.attachEvent) iframeId.attachEvent("onload", eventHandler);
+//   if (iframeId.addEventListener) iframeId.addEventListener("load", eventHandler, true);
+//   if (iframeId.attachEvent) iframeId.attachEvent("onload", eventHandler);
 
-  // Set properties of form...
-  form.setAttribute("target", "upload_iframe");
-  form.setAttribute("action", action_url);
-  form.setAttribute("method", "post");
-  form.setAttribute("enctype", "multipart/form-data");
-  form.setAttribute("encoding", "multipart/form-data");
+//   // Set properties of form...
+//   form.setAttribute("target", "upload_iframe");
+//   form.setAttribute("action", action_url);
+//   form.setAttribute("method", "post");
+//   form.setAttribute("enctype", "multipart/form-data");
+//   form.setAttribute("encoding", "multipart/form-data");
 
-  // Submit the form...
-  form.submit();
+//   // Submit the form...
+//   form.submit();
 
-  document.getElementById(div_id).innerHTML = "Uploading...";
-}
+//   document.getElementById(div_id).innerHTML = "Uploading...";
+// }
