9af409eecd102a13af338b93b60dcb39fe5e843e
jmacpherson <jd.macpherson@gmail.com>
2013-12-20 09:41:55 -0500

    added sized versions to image_uploader and set geomarker show to display different versions based upon window width at time of load

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,6 +36,14 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [200, 200]
   end
 
+  version :medium do
+    process :resize_to_fit => [300, 300]
+  end
+
+  version :large do
+    process :resize_to_fit => [400, 400]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
